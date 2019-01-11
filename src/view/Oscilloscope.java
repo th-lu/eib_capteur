f8e20d35998f7736c9fd474c930b2b07f811abd7
julie <julie@10.10.25.225>
2019-01-11 17:13:08 +0100

    correction erreur port usb

--- src/view/Oscilloscope.java
+++ src/view/Oscilloscope.java
@@ -1,11 +1,14 @@
 package view;
 	
+import javax.swing.JButton;
+
 import com.rapplogic.xbee.api.XBeeException;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import model.Signal;
 import model.SignalTools;
@@ -32,15 +35,15 @@ public class Oscilloscope extends Application {
 		try {
 			XBeeListener test = new XBeeListener();
 		} catch (XBeeException e) {
-			e.printStackTrace();
+			//e.printStackTrace()
+			System.out.println(e.getMessage());
 		}
-//		launch(args);
+		launch(args);
 	}
 	private Node createContent() {
 		Screen ecran = new Screen();
 		Signal sig = new Signal(SIGTYPE.SINE);
 		ecran.addSignal(sig);
-		//ecran.addSignal(sig);
 		return ecran;
 	}
 	/*public void stop() {
@@ -50,4 +53,6 @@ public class Oscilloscope extends Application {
 	/*public void init() {
 		
 	}*/
+	Button bouton = new Button("A");
+	
 }
